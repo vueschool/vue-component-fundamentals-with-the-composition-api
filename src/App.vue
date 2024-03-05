eca5f7242bc17aea20a9354882239c10ef1370c1
Daniel Kelly <me@danielkelly.io>
2024-03-05 12:12:19 -0600

    2-reusable-components-with-props

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,19 @@
 <script setup>
-import CounterButton from './components/CounterButton.vue'
+import CoffeePlan from './components/CoffeePlan.vue'
+import { ref } from 'vue'
+const plans = ref(['The Single', 'The Curious', 'The Addict', 'The Hacker'])
 </script>
 
 <template>
-  <counter-button />
-  <counter-button />
-  <counter-button />
-  <counter-button />
+  <div class="content">
+    <h1 class="title">Coffee Plans</h1>
+
+    <h2 class="subtitle">
+      We travel the world to source the very best single origin coffee for you
+    </h2>
+
+    <div class="plans">
+      <CoffeePlan v-for="plan in plans" :key="plan" :name="plan"></CoffeePlan>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/CoffeePlan.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  name: { type: String, default: 'Default Plan' }
+})
+</script>
+<template>
+  <div class="plan">
+    <div class="description">
+      <span class="title">{{ name }} </span>
+    </div>
+  </div>
+</template>
