f731e0740a4cfcf90d8342f847e374a6d7e265a9
Daniel Kelly <me@danielkelly.io>
2024-03-05 14:06:49 -0600

    4-nested-components-in-vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <script setup>
-import CoffeePlan from './components/CoffeePlan.vue'
-import { ref } from 'vue'
-const plans = ref(['The Single', 'The Curious', 'The Addict', 'The Hacker'])
+import PlanPicker from './components/PlanPicker.vue'
 </script>
 
 <template>
   <div class="content">
+    <PlanPicker />
     <h1 class="title">Coffee Plans</h1>
 
     <h2 class="subtitle">
       We travel the world to source the very best single origin coffee for you
     </h2>
 
-    <div class="plans">
-      <CoffeePlan v-for="plan in plans" :key="plan" :name="plan"></CoffeePlan>
-    </div>
+    <PlanPicker />
   </div>
 </template>
